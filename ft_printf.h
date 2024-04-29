00a69f7a20cc9720813b5432745db80f72dbdda9
foxy12112 <Leon.dick1@gmx.de>
2024-04-29 13:44:19 +0200

    milk

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:57:00 by ldick             #+#    #+#             */
-/*   Updated: 2024/03/20 11:04:15 by ldick            ###   ########.fr       */
+/*   Updated: 2024/04/29 13:44:06 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,20 @@ void	ft_print_pointers(void *p, int *milk);
 void	ft_print_str(char *s, int *milk);
 void	ft_print_uns_d(unsigned int n, int *milk);
 void	ft_print_hex_upper(unsigned int n, int *milk);
+/**
+ * @brief personal implementation of the function printf
+ * @param[in] format - the function on which to use after a %
+ * @note the functions available are
+ * @note %, which prints a %;
+ * @note c which prints a single character;
+ * @note s which prints a string of characters;
+ * @note p which prints a pointer address;
+ * @note d or i which prints a integer;
+ * @note u which prints an unsigned int
+ * @note x which prints a lowercase hexadecimal int
+ * @note X which prints a uppercase hexadecimal int
+ * @return returns a string with format applied to every defined character
+*/
 int		ft_printf(const char *format, ...);
 
 #endif
