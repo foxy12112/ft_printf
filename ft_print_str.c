c1b7a013b2ad4d9668343f388c25c3dc04abda3d
Leon Dick <ldick@1-E-3.42heilbronn.de>
2024-06-16 10:26:15 +0200

    removed print_fd from printf

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,23 +6,11 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 12:49:43 by ldick             #+#    #+#             */
-/*   Updated: 2024/03/19 13:39:40 by ldick            ###   ########.fr       */
+/*   Updated: 2024/06/16 10:25:25 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static void	ft_putstr_fd(char *s, int fd)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
-}
 
 void	ft_print_str(char *s, int *milk)
 {
